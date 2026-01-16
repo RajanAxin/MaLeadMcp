ec99f7907c717f19c90336ea4ea9ce2158e399d7
RajanAxin <rajan@axinenterprise.com>
2026-01-17 00:16:28 +0530

    ssws

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const app = express();
 
 app.use(express.json());
 
+// CORS + OPTIONS (CRITICAL)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
+
 app.get('/mcp/', (req, res) => {
   res.json({
     protocol: 'mcp',
